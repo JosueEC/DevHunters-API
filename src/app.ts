83e354da6614cf1f9eadeeaa7743c26613b06a15
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-09 16:39:36 -0600

    feat(app): 🔨 Add morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
+
+import professionalRoutes from './Professional/routes/professional.routes'
 
 const server = express()
 
@@ -8,9 +11,12 @@ server.use(cors({
 }))
 
 server.use(express.json())
+server.use(morgan('dev'))
 
-server.use('/', (_req, res) => {
-  res.status(200).send({ message: 'Server online' })
+server.use('/professional', professionalRoutes)
+
+server.use('*', (_req, res) => {
+  res.status(404).send({ message: 'NOT FOUND' })
 })
 
 export default server
