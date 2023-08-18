d8d62dae73905e811788ef40b5adba6123d7c6a0
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-08-17 20:26:24 -0600

    feat(controllers): 🔨 Add login controller function

--- src/User/controllers/auth.controller.ts
+++ src/User/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { httpSucces, httpError } from '../helpers/http.helper'
-import { registerUser } from '../services/auth.service'
+import { registerUser, loginUser } from '../services/auth.service'
 
 const register = ({ body }: Request, res: Response): void => {
   registerUser(body)
@@ -8,6 +8,13 @@ const register = ({ body }: Request, res: Response): void => {
     .catch(({ message }) => httpError(res, 400, message))
 }
 
+const login = ({ body }: Request, res: Response): void => {
+  loginUser(body)
+    .then((response) => httpSucces(res, 200, response))
+    .catch(({ message }) => httpError(res, 400, message))
+}
+
 export {
-  register
+  register,
+  login
 }
